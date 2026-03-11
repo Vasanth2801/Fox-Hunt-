15f2f9cc9576a2b2c533b8a30717c753bc71c786
Vasanth2801 <vasanthravichandran28@gmail.com>
2026-03-11 08:51:55 +0530

    Player Movement

---
+++ Fox Hunt/Assets/Scripts/Player/Player.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [Header("Movement Settings")]
+    [SerializeField] private float speed = 5f;
+
+    [Header("References")]
+    [SerializeField] private Rigidbody2D rb;
+
+    [Header("Inputs")]
+    [SerializeField] private float moveInput;
+
+
+    void Update()
+    {
+        moveInput = Input.GetAxis("Horizontal") * Time.deltaTime;
+    }
+
+    void FixedUpdate()
+    {
+        Move();
+    }
+
+    void Move()
+    {
+        rb.linearVelocity = new Vector2(moveInput * speed, rb.linearVelocity.y);
+    }
+}
